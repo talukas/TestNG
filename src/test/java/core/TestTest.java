9a3a7a2fbac9a8b4c5ddd1bc2962b77e4b6c86b1
Tania <taluk@outlook.com>
2017-05-27 11:29:25 -0700

    correction

--- src/test/java/core/TestTest.java
+++ src/test/java/core/TestTest.java
@@ -1,13 +1,13 @@
 package core;
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
+import static org.testng.Assert.assertEquals;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
 	@Test(enabled = true)
 	public void test() {
-		AssertJUnit.assertEquals("abc", "abc", "Error message");
+	assertEquals("abc", "abc", "Error message");
 	} //method (test)
 } //class
